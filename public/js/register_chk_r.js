7520a2d34abffc71801e1c8954b741d453e9f3d0
t14henry <t14henry@gmail.com>
2011-09-21 11:10:34 +0800

    add email event handler

--- public/js/register_chk_r.js
+++ public/js/register_chk_r.js
@@ -1,2 +1,3 @@
 document.getElementById("password").onblur = check_psw_length;
 document.getElementById("c_password").onblur = check_psw_equal;
+document.getElementById("email").onblur = check_email;
